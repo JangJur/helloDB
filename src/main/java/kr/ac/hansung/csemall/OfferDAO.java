b640bdee000d57e95926627b2e23f9b88b6922e2
JJ <wkdwndud753@naver.com>
2018-12-16 23:09:20 +0900

    offerDAO - Annotation

--- src/main/java/kr/ac/hansung/csemall/OfferDAO.java
+++ src/main/java/kr/ac/hansung/csemall/OfferDAO.java
@@ -2,12 +2,16 @@ package kr.ac.hansung.csemall;
 
 import javax.sql.DataSource;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
 
+@Component("offerDAO")
 public class OfferDAO {
 	
 	private JdbcTemplate jdbcTemplate;
 	
+	@Autowired
 	public void setDataSource(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
